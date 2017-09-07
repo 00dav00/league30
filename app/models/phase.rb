e8d45b0bdc2479d755cc3a80d7ccc5b0a376b32e
David Revelo <drevelo@stackbuilders.com>
2017-09-07 18:56:07 -0500

    First phase validation

--- app/models/phase.rb
+++ app/models/phase.rb
@@ -1,2 +1,10 @@
 class Phase < ApplicationRecord
+  TYPES = [
+    :default,
+    :cumulative,
+    :group_elimination,
+    :single_elimination
+  ]
+
+  validates :type, inclusion: { in: Phase::TYPES }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170825043650) do
+ActiveRecord::Schema.define(version: 20170907231911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "phases", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "position_in_tournament", null: false
+    t.string "type", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tournaments", force: :cascade do |t|
     t.string "name", null: false
