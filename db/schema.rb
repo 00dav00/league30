1e89bdc9ad74ea0c4a8dfedc5ba2be533b690c63
David Revelo <david@aperturaradical.org>
2017-09-13 21:37:55 -0500

    Add teams table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908042357) do
+ActiveRecord::Schema.define(version: 20170914023206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 20170908042357) do
     t.datetime "updated_at", null: false
     t.bigint "tournament_id"
     t.index ["tournament_id"], name: "index_phases_on_tournament_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "short_name", null: false
+    t.string "name", null: false
+    t.string "full_name", null: false
+    t.string "logo_image", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tournaments", force: :cascade do |t|
